6aebc3b2a21a07faa25b26deef87090f85728c40
tantrungnguyen2001 <trungnt@zigvy.com>
2024-03-04 18:22:45 +0700

    update navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -34,7 +34,7 @@ const Links: LinksType = [
 const Nav = () => {
   const [isShowNav, setIsShowNav] = useState(true);
 
-  const handleShowNav = (): void => {
+  const handleShowNav = () => {
     setIsShowNav(!isShowNav);
   };
 
@@ -43,20 +43,22 @@ const Nav = () => {
       <div className="md:flex items-center justify-between bg-white py-4">
         <img src={logo} alt="logo icon" />
 
-        {isShowNav && (
-          <ul className="font-medium md:text-navAndText w-full md:w-auto h-screen md:h-auto bg-navMobile md:bg-inherit absolute md:static top-0 right-0">
-            <div className="flex flex-col md:flex-row gap-4 md:gap-8 w-2/3 md:w-auto h-full md:h-auto ml-auto pt-32 md:pt-0 pl-8 md:pl-0 bg-Offwhite">
-              {Links.map((link, i) => (
-                <li key={i} className="hover:text-btn">
-                  <a href={link.link}>{link.name}</a>
-                </li>
-              ))}
-            </div>
-          </ul>
-        )}
+        <ul
+          className={`font-medium md:text-navAndText w-full md:w-auto h-screen md:h-auto bg-navMobile md:bg-inherit absolute md:static top-0 ${
+            isShowNav ? "right-[100%]" : "right-0"
+          }`}
+        >
+          <div className="flex flex-col md:flex-row gap-4 md:gap-8 w-2/3 md:w-auto h-full md:h-auto ml-auto pt-32 md:pt-0 pl-8 md:pl-0 bg-Offwhite">
+            {Links.map((link, i) => (
+              <li key={i} className="hover:text-btn">
+                <a href={link.link}>{link.name}</a>
+              </li>
+            ))}
+          </div>
+        </ul>
       </div>
       <img
-        src={isShowNav ? closeMenuIcon : menuIcon}
+        src={isShowNav ? menuIcon : closeMenuIcon}
         alt="menu"
         onClick={handleShowNav}
         className="w-[40px] aspect-square absolute top-4 right-8 block md:hidden z-10"
